48331abcfbc5a637f8910c026ed9abee1c91c8ba
aedans <aedan.smith@gmail.com>
2024-02-21 15:30:41 -0800

    Switch to systemd-boot

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -42,9 +42,8 @@ in
   programs.fish.enable = true;
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
