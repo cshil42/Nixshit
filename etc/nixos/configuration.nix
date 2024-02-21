33e324f422dedc850555b76d583358a1ccaa80ae
aedans <aedan.smith@gmail.com>
2024-02-20 23:01:31 -0800

    Add gnome config

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -17,6 +17,26 @@ in
   home-manager.users.hans = {
     home.stateVersion = "23.11";
     programs.fish.enable = true;
+
+    dconf = {
+      enable = true;
+
+      settings = {
+        "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
+        "org/gnome/desktop/wm/keybindings" = {
+          move-to-workspace-1 = [ "<Shift><Alt>1" ];
+          move-to-workspace-2 = [ "<Shift><Alt>2" ];
+          move-to-workspace-3 = [ "<Shift><Alt>3" ];
+          move-to-workspace-4 = [ "<Shift><Alt>4" ];
+
+          switch-to-workspace-1 = [ "<Alt>1" ];
+          switch-to-workspace-2 = [ "<Alt>2" ];
+          switch-to-workspace-3 = [ "<Alt>3" ];
+          switch-to-workspace-4 = [ "<Alt>4" ];
+        };
+      };
+    };
   };
 
   programs.fish.enable = true;
