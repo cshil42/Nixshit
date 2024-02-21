288fa4df74970cfc206089de080b7ca29fd28bc6
aedans <aedan.smith@gmail.com>
2024-02-21 15:56:25 -0800

    Disable auto-login

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -174,10 +174,6 @@ in
     ];
   };
 
-  # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "hans";
-
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
