98bfa2bf5a2afbb058139cfdcb756fa9ff3b9cc6
aedans <aedan.smith@gmail.com>
2024-03-04 18:50:26 -0800

    Support modules

--- README.md
+++ README.md
@@ -3,5 +3,22 @@
 ```bash
 nix-shell -p git --command "git clone https://github.com/aedans/.dotfiles.git"
 cd .dotfiles
-sudo bash ./bootstrap.sh
+```
+
+## VirtualBox
+
+```bash
+sudo bash bootstrap.sh grub
+```
+
+## PC
+
+```bash
+sudo bash bootstrap.sh systemd-boot
+```
+
+## PC with NVIDIA
+
+```bash
+sudo bash bootstrap.sh systemd-boot nvidia
 ```
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-ln -sf "$(pwd)/etc/nixos/configuration.nix" /etc/nixos/configuration.nix
+set -e
+
+DIR="$( dirname -- "${BASH_SOURCE[0]}"; )";   # Get the directory name
+DIR="$( realpath -e -- "$DIR"; )";    # Resolve its full path if need be
+DIR="$(dirname "$DIR")"
+
+rm /etc/nixos/configuration.nix
+
+text="{
+  imports = [
+    $DIR/.dotfiles/shared.nix
+$(for i in "$@"; do echo "    $DIR/.dotfiles/modules/$i.nix"; done)
+  ];
+}"
+
+echo "$text" > /etc/nixos/configuration.nix
 
 nixos-rebuild switch
 
