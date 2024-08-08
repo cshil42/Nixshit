d10d4f8053c0f0da7eddcb175fce4ecbb78eb850
aedans <aedan.smith@gmail.com>
2024-08-08 00:17:55 -0700

    Add to git before commit

--- commit.sh
+++ commit.sh
@@ -2,4 +2,5 @@
 
 NIXOS_LABEL="$(echo $1 | tr " " "-" | tr '[:upper:]' '[:lower:]')" nixos-rebuild switch
 GENERATION="$(nix-env --list-generations --profile /nix/var/nix/profiles/system | grep current | awk '{print $1}')"
+git add .
 git commit -m "$1 ($GENERATION)"
