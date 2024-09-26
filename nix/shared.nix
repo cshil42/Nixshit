d0ec18d313ad799d0496d91ed963b4bceee245a3
aedans <aedan.smith@gmail.com>
2024-09-26 14:29:15 -0700

    Actually let's hold off for now

--- nix/shared.nix
+++ nix/shared.nix
@@ -170,7 +170,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-    nodejs_22
+    nodejs
     wineWowPackages.stable
     winetricks
     rclone
