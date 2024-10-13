9fddcb64a6c590d702fa9233192dd0fc1e967135
Aedan Smith <aedan.smith@outlook.com>
2024-10-13 01:10:44 -0700

    Fix dump

--- dump.sh
+++ dump.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 for in in /org/gnome/shell/ /org/gnome/desktop/ /org/gnome/mutter/; do
-    echo Dumping $in to /home/hans/.dotfiles/nix/dconf/${in////-}.nix
-    nix-shell -p dconf2nix --command "dconf dump $in | dconf2nix --root $in > /home/hans/.dotfiles/nix/dconf/${in////-}.nix"
+    echo Dumping $in to /home/hans/.dotfiles/nix/de/dconf/${in////-}.nix
+    nix-shell -p dconf2nix --command "dconf dump $in | dconf2nix --root $in > /home/hans/.dotfiles/nix/de/dconf/${in////-}.nix"
 done
 
 echo Dumping vscode extensions
