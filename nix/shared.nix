4c766afc48bda811afe36f837ac77eb530adc4ca
Aedan Smith <aedan.smith@outlook.com>
2024-11-26 03:52:14 -0800

    Add swap

--- nix/shared.nix
+++ nix/shared.nix
@@ -145,6 +145,13 @@
     package = pkgs.jdk21;
   };
 
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 4096;
+    }
+  ];
+
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
