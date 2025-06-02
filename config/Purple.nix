cdffaca641e27954bd6755bb045f49f40f9bf818
cshil42 <connorshilmover@gmail.com>
2025-06-01 19:35:58 -0700

    amd

--- config/Purple.nix
+++ config/Purple.nix
@@ -2,7 +2,7 @@
   imports = [ 
     /home/connor/.dotfiles/nix/shared.nix
     /home/connor/.dotfiles/nix/boot/systemd.nix
-    /home/connor/.dotfiles/nix/gpu/nvidia.nix
+    #/home/connor/.dotfiles/nix/gpu/nvidia.nix
     # /home/connor/.dotfiles/nix/backup/usb.nix
   ];
 
